8f606229faf5e5560eb8babeb4ba319adfc44c21
Brian L. Troutwine <brian@troutwine.us>
2022-01-03 10:40:04 -0800

    Update the observer to collect prometheus metrics directly
    We would like to be able to determine if soak tests are erratic automatically,
    REF #10606. Also in #10627 we find that while it's possible to export
    cummulative bytes written from the kube it's kind of a crummy experience
    overall.
    
    This commit starts to address both by adjusting the observer to natively
    understand prometheus, allowing us to export mostly-raw data from the kube for
    later analysis.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- lib/soak/src/bin/observer.rs
+++ lib/soak/src/bin/observer.rs
@@ -1,13 +1,19 @@
 //! `observer` is a program that inspects a prometheus with a configured query,
-//! writes the result out to disk. It replaces curl-in-a-loop in our soak infra.
-use std::{borrow::Cow, fs, io::Read, time::Duration};
-
+//! writes the result out to disk.
 use argh::FromArgs;
+use prometheus_parser::GroupKind;
 use reqwest::Url;
 use serde::Deserialize;
 use snafu::Snafu;
+use std::{
+    borrow::Cow,
+    fs,
+    io::{Read, Write},
+    time::Duration,
+};
 use tokio::{runtime::Builder, time::sleep};
-use tracing::{debug, error, info};
+use tracing::{debug, error};
+use uuid::Uuid;
 
 fn default_config_path() -> String {
     "/etc/vector/soak/observer.yaml".to_string()
@@ -21,27 +27,22 @@ struct Opts {
     config_path: String,
 }
 
-/// Query configuration
+/// Target configuration
 #[derive(Debug, Deserialize)]
-pub struct Query {
+pub struct Target {
     id: String,
-    query: String,
-    unit: soak::Unit,
+    url: String,
 }
 
 /// Main configuration struct for this program
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    /// Location to scrape prometheus
-    pub prometheus: String,
     /// The name of the experiment being observed
     pub experiment_name: String,
     /// The variant of the experiment, generally 'baseline' or 'comparison'
     pub variant: soak::Variant,
-    /// The vector ID associated with the experiment
-    pub vector_id: String,
-    /// The queries to make of the experiment
-    pub queries: Vec<Query>,
+    /// The targets for this experiment.
+    pub targets: Vec<Target>,
     /// The file to record captures into
     pub capture_path: String,
 }
@@ -58,6 +59,24 @@ enum Error {
     ParseFloat { error: std::num::ParseFloatError },
     #[snafu(display("Could not serialize output: {}", error))]
     Json { error: serde_json::Error },
+    #[snafu(display("Could not deserialize config: {}", error))]
+    Yaml { error: serde_yaml::Error },
+    #[snafu(display("Could not deserialize prometheus response: {}", error))]
+    Prometheus {
+        error: prometheus_parser::ParserError,
+    },
+}
+
+impl From<serde_yaml::Error> for Error {
+    fn from(error: serde_yaml::Error) -> Self {
+        Self::Yaml { error }
+    }
+}
+
+impl From<prometheus_parser::ParserError> for Error {
+    fn from(error: prometheus_parser::ParserError) -> Self {
+        Self::Prometheus { error }
+    }
 }
 
 impl From<reqwest::Error> for Error {
@@ -97,91 +116,73 @@ enum Status {
     Error,
 }
 
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct QueryResultValue {
-    time: f64,
-    value: String,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct QueryResult {
-    value: QueryResultValue,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct QueryData {
-    result: Vec<QueryResult>,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct QueryResponse {
-    data: QueryData,
-}
-
 struct Worker {
-    vector_id: String,
     experiment_name: String,
     variant: soak::Variant,
-    queries: Vec<(Query, Url)>,
+    targets: Vec<(Target, Url)>,
     capture_path: String,
 }
 
 impl Worker {
     fn new(config: Config) -> Self {
-        let mut queries = Vec::with_capacity(config.queries.len());
-        for query in config.queries {
-            let url = format!("{}/api/v1/query?query={}", config.prometheus, query.query);
-            let url = url.parse::<Url>().unwrap();
-            queries.push((query, url));
+        let mut targets = Vec::with_capacity(config.targets.len());
+        for target in config.targets {
+            let url = target.url.parse::<Url>().expect("failed to parse URL");
+            targets.push((target, url));
         }
 
         Self {
-            vector_id: config.vector_id,
             experiment_name: config.experiment_name,
             variant: config.variant,
-            queries,
+            targets,
             capture_path: config.capture_path,
         }
     }
 
     async fn run(self) -> Result<(), Error> {
+        let run_id: Uuid = Uuid::new_v4();
         let client: reqwest::Client = reqwest::Client::new();
 
-        let file = fs::File::create(self.capture_path)?;
-        let mut wtr = csv::Writer::from_writer(file);
+        let mut wtr = fs::File::create(self.capture_path)?;
         for fetch_index in 0..u64::max_value() {
-            for (query, url) in &self.queries {
+            for (target, url) in &self.targets {
+                // TODO must handle failures correctly
                 let request = client.get(url.clone()).build()?;
-                let body = client
-                    .execute(request)
-                    .await?
-                    .json::<QueryResponse>()
-                    .await?;
-                debug!("body: {:?}", body.data);
+                let body = client.execute(request).await?.text().await?;
+                let metric_groups = prometheus_parser::parse_text(&body)?;
 
-                if !body.data.result.is_empty() {
-                    let time = body.data.result[0].value.time;
-                    let value = body.data.result[0].value.value.parse::<f64>()?;
-                    let output = soak::Output {
-                        experiment: Cow::Borrowed(&self.experiment_name),
-                        variant: self.variant,
-                        vector_id: Cow::Borrowed(&self.vector_id),
-                        time,
-                        query_id: Cow::Borrowed(&query.id),
-                        query: Cow::Borrowed(&query.query),
-                        value,
-                        unit: query.unit,
-                        fetch_index,
+                if metric_groups.is_empty() {
+                    error!("failed to request body: {:?}", body);
+                }
+
+                for metric in metric_groups {
+                    let metric_name = metric.name;
+                    let (kind, mm) = match metric.metrics {
+                        GroupKind::Summary(..)
+                        | GroupKind::Histogram(..)
+                        | GroupKind::Untyped(..) => continue,
+                        GroupKind::Counter(mm) => (soak::MetricKind::Counter, mm),
+                        GroupKind::Gauge(mm) => (soak::MetricKind::Gauge, mm),
                     };
-                    info!("{}", serde_json::to_string(&output)?);
-                    wtr.serialize(&output).expect("could not serialize");
-                    wtr.flush()?;
-                } else {
-                    error!("failed to request body: {:?}", body.data);
+                    for (k, v) in mm.iter() {
+                        let timestamp = k.timestamp;
+                        let value = v.value;
+                        let output = soak::Output {
+                            run_id: Cow::Borrowed(&run_id),
+                            experiment: Cow::Borrowed(&self.experiment_name),
+                            variant: self.variant,
+                            target: target.id.clone(),
+                            time: timestamp,
+                            fetch_index,
+                            metric_name: metric_name.clone(),
+                            metric_kind: kind,
+                            metric_labels: k.labels.clone(),
+                            value,
+                        };
+                        serde_json::to_writer(&mut wtr, &output).expect("could not serialize json");
+                        wtr.write_all(b"\n")?;
+                        wtr.flush()?;
+                    }
                 }
             }
             sleep(Duration::from_secs(1)).await;
@@ -189,26 +190,26 @@ impl Worker {
         // SAFETY: The only way to reach this point is to break the above loop
         // -- which we do not -- or to traverse u64::MAX seconds, implying that
         // the computer running this program has been migrated away from the
-        // Earth meanwhile as our dear craddle is now well encompassed by the
+        // Earth meanwhile as our dear cradle is now well encompassed by the
         // sun. Unless we moved the Earth.
         unreachable!()
     }
 }
 
-fn get_config() -> Config {
+fn get_config() -> Result<Config, Error> {
     let ops: Opts = argh::from_env();
     let mut file: std::fs::File = std::fs::OpenOptions::new()
         .read(true)
-        .open(ops.config_path)
-        .unwrap();
+        .open(ops.config_path)?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    serde_yaml::from_str(&contents).unwrap()
+    file.read_to_string(&mut contents)?;
+    serde_yaml::from_str(&contents).map_err(|e| e.into())
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
     tracing_subscriber::fmt().init();
-    let config: Config = get_config();
+    let config: Config = get_config()?;
+    debug!("CONFIG: {:?}", config);
     let runtime = Builder::new_current_thread()
         .enable_time()
         .enable_io()
@@ -216,4 +217,5 @@ fn main() {
         .unwrap();
     let worker = Worker::new(config);
     runtime.block_on(worker.run()).unwrap();
+    Ok(())
 }
